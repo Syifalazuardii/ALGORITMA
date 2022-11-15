aa0e222570548f428cc9b0d67b8cff13edcbbffd
Syifalazuardii <118255569+Syifalazuardii@users.noreply.github.com>
2022-11-15 17:05:40 +0700

    Add files via upload

---
+++ arraydimensi1 syifa.cpp
@@ -0,0 +1,17 @@
+//TUGAS ARRAY 1 DIMENSI
+//SYIFA LAZUARDI AKBAR
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+     int Angka[5]={84,46,57,1,21};
+
+     for(int a=0; a<5;a++){
+          cout<<"Tampil nilai indek ke ["<<a<<"] = "<<Angka[a];
+          cout<<endl;
+     }
+
+     return 0;
+}
